eb459efbd05ddcea15bc1e7b1dc62290cf240b90
romanse <semrom@mail.ru>
2019-09-08 17:44:07 +0300

    add swagger

--- javaee-learning-server/src/main/java/ru/semenov/RestAppConfig.java
+++ javaee-learning-server/src/main/java/ru/semenov/RestAppConfig.java
@@ -3,6 +3,8 @@ package ru.semenov;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+
+// OpenAPI URL: http://localhost:8080/javaee-learning/rest/openapi.json
 @ApplicationPath("/rest")
 public class RestAppConfig extends Application {
 
